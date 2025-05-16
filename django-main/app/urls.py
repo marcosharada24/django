0c2cc18e14e24a57e8cea91d8f0f03ab4d126ac4
marcosharada24 <marcos.anna@fatec.sp.gov.br>
2025-05-16 09:17:00 -0300

    Update urls.py

--- django-main/app/urls.py
+++ django-main/app/urls.py
@@ -11,6 +11,14 @@ urlpatterns = [
   path('exluir-usuario/<int:id_usuario>', views.excluirUsuario, name="excluirUsuario"),
 
   path('editar-usuario/<int:id_usuario>', views.editarUsuario, name="editarUsuario"),
+  
+    path('produtos/', views.listar_produtos, name='listar_produtos'),
+  
+    path('produtos/novo/', views.cadastrar_produto, name='cadastrar_produto'),
+  
+    path('produtos/editar/<int:id>/', views.editar_produto, name='editar_produto'),
+  
+    path('produtos/excluir/<int:id>/', views.excluir_produto, name='excluir_produto'),
 
-  path('cadastrarProduto', views.cadastrarProduto, name="cadastrarProduto"),
+
 ]
